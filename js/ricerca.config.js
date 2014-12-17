0f3600b558d69fed47c1c775346545a5716980d0
Jackos <jackos.dev@gmail.com>
2014-12-17 18:36:44 +0100

    Independant configuration file
    No more ugly code to look at!

---
+++ js/ricerca.config.js
@@ -0,0 +1,9 @@
+
+ricercaProvidersList = [
+    "!d",   "DuckDuckGo",   "https://www.duckduckgo.com",           "q",
+    "!g",   "Google",       "https://www.google.com/search",        "q",
+    "!w",   "Wikipedia",    "http://en.wikipedia.org/w/index.php",  "search",
+    "!y",   "YouTube",      "https://www.youtube.com/results",      "search_query"
+];
+
+ricercaDefaultProvider = "DuckDuckGo";
